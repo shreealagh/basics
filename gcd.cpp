a681e21f8b700428d4761b119d51b4800c2caa79
shreealagh <104250062+shreealagh@users.noreply.github.com>
2022-06-05 18:05:25 +0530

    Create gcd.cpp

---
+++ gcd.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n1,n2;
+    cin>>n1>>n2;
+    int i=1;
+    int gcd=0;
+    while(i<=n1 && i<=n2)
+    {
+        if(n1%i==0 && n2%i==0)
+        {
+           gcd=i;
+        }
+        i++;
+    }
+    cout<<gcd<<endl;
+}
